89a6f1996c95fb1ababe65a28824780bac660948
jon.escamilla <jonathanescamilla1@gmail.com>
2020-12-26 10:55:20 -0800

    abstract auto mute feature outside of client.ts

---
+++ src/autoMute.ts
@@ -0,0 +1,38 @@
+import Discord from 'discord.js';
+
+let memberUnmute: NodeJS.Timeout;
+/**
+ * Will auto mute members that join a voice channel for `time` amount
+ * @param oldState Previous user VoiceState
+ * @param newState Current user VoiceState
+ * @param time the desired time a user should wait when joining a voice channel
+ */
+export const autoMute = (
+  oldState: Discord.VoiceState,
+  newState: Discord.VoiceState,
+  time: number
+): void => {
+  const oldChannel = oldState.channelID;
+  const newChannel = newState.channelID;
+
+  // if the user leaves a channel clear the setTimeout
+  if (oldChannel !== newChannel && oldChannel !== null) {
+    clearTimeout(memberUnmute);
+  }
+
+  if (
+    // if the user joined a new channel
+    (oldChannel === null && newChannel !== null) ||
+    // the user switched channels
+    (oldChannel !== newChannel &&
+      newChannel !== null &&
+      newState.channelID == '785293840298803241')
+  ) {
+    // mute the member on voice join
+    newState.member?.voice.setMute(true);
+    // assign the variable to unmute
+    memberUnmute = setTimeout(function () {
+      oldState.member?.voice.setMute(false);
+    }, time);
+  }
+};
--- src/client.ts
+++ src/client.ts
@@ -1,4 +1,5 @@
 import Discord from 'discord.js';
+import { autoMute } from './autoMute';
 const client = new Discord.Client();
 require('dotenv').config();
 
@@ -9,36 +10,7 @@ client.on('ready', () => {
 });
 
 client.on('voiceStateUpdate', (oldState, newState) => {
-  autoMute(oldState, newState);
+  autoMute(oldState, newState, 3000);
 });
-
-let memberUnmute: NodeJS.Timeout;
-
-const autoMute = (
-  oldState: Discord.VoiceState,
-  newState: Discord.VoiceState
-) => {
-  const oldChannel = oldState.channelID;
-  const newChannel = newState.channelID;
-
-  // if the user leaves a channel clear the setTimeout
-  if (oldChannel !== newChannel && oldChannel !== null) {
-    clearTimeout(memberUnmute);
-  }
-
-  if (
-    // if the user joined a new channel
-    (oldChannel === null && newChannel !== null) ||
-    // the user switched channels
-    (oldChannel !== newChannel && newChannel !== null)
-  ) {
-    // mute the member on voice join
-    newState.member?.voice.setMute(true);
-    // assign the variable to unmute
-    memberUnmute = setTimeout(function () {
-      oldState.member?.voice.setMute(false);
-    }, 5000);
-  }
-};
 
 client.login(DISCORD_BOT_TOKEN);
