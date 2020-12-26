74a7251c5f4bc2d49748417a0af74f9212e942cd
jon.escamilla <jonathanescamilla1@gmail.com>
2020-12-26 15:47:01 -0800

    variables file

---
+++ src/variables.ts
@@ -0,0 +1,24 @@
+/**
+ * used in helper functions to handle logic based on ranking
+ */
+export const rankings = {
+  /** Council*/
+  rank_500: 'Council',
+  /** Nominated to Council*/
+  rank_490: 'Nominated to Council',
+  /** Senior Member*/
+  rank_400: 'Senior Member',
+  /** Nominated to Senior Member*/
+  rank_390: 'Nominated to Senior Member',
+  /** Member*/
+  rank_300: 'Member',
+  /** Trusted*/
+  rank_200: 'Trusted',
+  /** Newbie*/
+  rank_100: 'Newbie',
+};
+
+/**
+ * prefix variable
+ */
+export const prefix = '!';
